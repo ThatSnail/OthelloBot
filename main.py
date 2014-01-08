65159b750111c2d8450d6b709ef00a6693884115
Roy Tu <thatsnail@gmail.com>
2014-01-07 20:58:58 -0500

    Stuff

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 import time
-import pygame
 from game import Game
 from math import floor
 from human_player import HumanPlayer
 from computer_player import ComputerPlayer
+#import pygame
+from widget import Widget
+
+USING_PYGAME = False
 
 screen = None
 game = None
@@ -17,11 +20,15 @@ color_green = (0, 128, 0)
 
 def main():
     # Init
-    pygame.init()
-    global screen, game, players
-    screen = pygame.display.set_mode((SW, SH))
-    screen.fill(color_green)
+    if USING_PYGAME:
+        pygame.init()
+        global screen
+        screen = pygame.display.set_mode((SW, SH))
+        screen.fill(color_green)
+    else:
+        widget = Widget(SW, SH)
 
+    global game, players
     game = Game()
     players = []
     players.append(HumanPlayer(game, Game.BLACK))
@@ -52,29 +59,32 @@ def event():
                     break
 
 def draw():
-    global screen
+    if USING_PYGAME:
+        global screen
 
-    # Draw screen
-    screen.fill(color_green)
-    for x in range(0, SW, floor(SW / 8)):
-        pygame.draw.line(screen, color_black, (x, 0), (x, SH))
-    for y in range(0, SH, floor(SH / 8)):
-        pygame.draw.line(screen, color_black, (0, y), (SW, y))
+        # Draw screen
+        screen.fill(color_green)
+        for x in range(0, SW, floor(SW / 8)):
+            pygame.draw.line(screen, color_black, (x, 0), (x, SH))
+        for y in range(0, SH, floor(SH / 8)):
+            pygame.draw.line(screen, color_black, (0, y), (SW, y))
 
-    # Draw pieces
-    for (x, y) in [(x, y) for x in range(8) for y in range(8)]:
-        {
-            Game.NONE: lambda: None,
-            Game.BLACK: lambda: pygame.draw.circle(screen, color_black, (x * SP + SP // 2, y * SP + SP // 2), SP // 2),
-            Game.WHITE: lambda: pygame.draw.circle(screen, color_white, (x * SP + SP // 2, y * SP + SP // 2), SP // 2)
-        }[game.state[x][y]]()
+        # Draw pieces
+        for (x, y) in [(x, y) for x in range(8) for y in range(8)]:
+            {
+                Game.NONE: lambda: None,
+                Game.BLACK: lambda: pygame.draw.circle(screen, color_black, (x * SP + SP // 2, y * SP + SP // 2), SP // 2),
+                Game.WHITE: lambda: pygame.draw.circle(screen, color_white, (x * SP + SP // 2, y * SP + SP // 2), SP // 2)
+            }[game.state[x][y]]()
     
-    # Draw liberties
-    for (x, y) in game.liberties():
-        pygame.draw.circle(screen, color_white, (x * SP + SP // 2, y * SP + SP // 2), 4)
+        # Draw liberties
+        for (x, y) in game.liberties():
+            pygame.draw.circle(screen, color_white, (x * SP + SP // 2, y * SP + SP // 2), 4)
 
-    # Update
-    pygame.display.update()
+        # Update
+        pygame.display.update()
+    else:
+        widget.draw(game)
 
 if __name__ == "__main__":
     main()
