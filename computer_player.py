6b3d16377c3666d77e056fce22e08dda0913550c
Roy Tu <thatsnail@gmail.com>
2014-01-10 14:48:42 -0500

    Changed move, added simulate_move, libmin strategy

--- computer_player.py
+++ computer_player.py
@@ -10,6 +10,6 @@ class ComputerPlayer(Player):
         # Calculate move
         liberties = self.game.liberties(self.player, self.state)
         heuristic = RandomStrategy.apply(self.player, self.state, liberties)
-        heuristic = CornerStrategy.apply(self.player, self.state, liberties, heuristic)
+        heuristic = CornerStrategy.apply(self.player, self.state, liberties, heuristic=heuristic)
         x, y = Strategy.run(heuristic)
         self.game.move(x, y)
--- strategy_random.py
+++ strategy_random.py
@@ -2,11 +2,11 @@ from random import random
 from strategy import Strategy
 
 class RandomStrategy(Strategy):
-    def apply(player, state, liberties, heuristic=None):
+    def apply(player, state, liberties, mult=1, heuristic=None):
         if heuristic == None:
-            heuristic = [(pos, 0) for pos in liberties[:]]
+            heuristic = [(pos, 1) for pos in liberties[:]]
         for i in range(len(heuristic)):
             pos, heur = heuristic[i]
             v = random()
-            heuristic[i] = (pos, heur + v)
+            heuristic[i] = (pos, heur * v ** mult)
         return heuristic
