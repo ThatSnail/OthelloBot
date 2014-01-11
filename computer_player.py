a5a859b53a61891f132a39ef71b8e4592223f2e1
Roy Tu <thatsnail@gmail.com>
2014-01-10 23:39:01 -0500

    where is my currying :(

--- computer_player.py
+++ computer_player.py
@@ -10,8 +10,11 @@ class ComputerPlayer(Player):
         Player.make_move(self, x, y)
         # Calculate move
         liberties = Game.liberties(self.player, self.state)
-        heuristic = RandomStrategy.apply(self.player, self.state, liberties)
-        heuristic = CornerStrategy.apply(self.player, self.state, liberties, heuristic=heuristic)
-        heuristic = LibminStrategy.apply(self.player, self.state, liberties, heuristic=heuristic)
+        def apply_strat(strategy, mult=1, heuristic=None):
+            return strategy.apply(self.player, self.state, mult, heuristic=heuristic)
+        heuristic = None
+        heuristic = apply_strat(RandomStrategy, 1, heuristic)
+        heuristic = apply_strat(CornerStrategy, 1, heuristic)
+        heuristic = apply_strat(LibminStrategy, 1, heuristic)
         x, y = Strategy.run(heuristic)
         self.game.move(x, y)
